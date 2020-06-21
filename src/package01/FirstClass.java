4841d3fe5a528fc4b745a5d710d8105201ee5eef
Kerim5858 <kerimanulur@gmail.com>
2020-06-21 15:33:23 -0400

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Ilk commit from local repostory");
 		System.out.println("ikinci commit from local repostory");
+		System.out.println("yeni branch olusturdum adi feature");
 
 	}
 
